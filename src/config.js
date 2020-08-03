86c85ada10f32fe4f62bcc2522f33c906a2ab789
sfuchsberger <slf0031@auburn.edu>
2020-08-02 23:18:23 -0500

    setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-taking-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://9i9pz1lav6.execute-api.us-east-2.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_0E6lhspF1",
+    APP_CLIENT_ID: "4h0mvkrds01jc3ut37ugsp71cn",
+    IDENTITY_POOL_ID: "us-east-2:0b8366b4-cd52-4aa7-9722-3cb89a057de"
+  }
+};
